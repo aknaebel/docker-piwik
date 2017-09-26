37dcea7d26009ac5e3381c0f3d93f5dcfb39055f
aknaebel <alain.knaebel@aknaebel.fr>
2017-09-26 08:31:33 +0200

    [change] correction rights on the piwik directory

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,8 +8,7 @@ if [ ! -e piwik.php ]; then
         tar cf - --one-file-system -C /tmp/piwik . | tar xf -
 fi
 
-chown -R root:${htuser} ${path}/
-chown -R ${htuser}:${htuser} ${path}/*
+chown -R ${htuser}:${htuser} ${path}
 
 echo >&2 "Update GeoIP base"
 curl -fsSL -o /var/www/piwik/misc/GeoIPCity.dat.gz http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
